ea9f45fed7d9ce3eb3ced0d6c609262477d1c938
ajmal4710 <ajmal4710@gmail.com>
2025-11-24 20:17:01 +0000

    "Fix: remove corrupted block file"

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import App from '../App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-ReactDOM.render(
+// React 18+ uses createRoot instead of render
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
